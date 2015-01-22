ebb91e57ae8f2cbd9ed6763b742b4cefb542e672
topejee <tommipenttila@outlook.com>
2015-01-23 00:54:37 +0200

    deadline

---
+++ 2DHyppelypeli/src/main/java/dhyppelypeli/Oliot/Vihollinen.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dhyppelypeli.Oliot;
+
+/**
+ *
+ * @author Tommi
+ */
+public class Vihollinen extends Hahmo{
+
+}
